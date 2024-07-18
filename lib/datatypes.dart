162f581dff4d64c891d3fd6767fc82a683fcb7cc
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 21:44:44 +0300

    Lists restricting datatype using generic typing

--- lib/datatypes.dart
+++ lib/datatypes.dart
@@ -1,5 +1,5 @@
 void main() {
-  List bookTitles = ["Book 1", "Book 2", "Book 3"];
-  bookTitles.add(2);
+  List<String> bookTitles = ["Book 1", "Book 2", "Book 3"];
+  //bookTitles.add(2);
   print(bookTitles); // list of dynamic(can hold a value of any datatype)
 }
