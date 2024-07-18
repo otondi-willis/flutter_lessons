5b2abfdf294ae054b3239b9203f99e2852a5b665
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 22:15:47 +0300

    use of functions with optional positional arguments

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,8 +1,12 @@
 void main(List<String> args) {
   print("Hello world");
   printCost(10.5, "Chocolate Bar");
+  printCost1("Willis",10.5);
 }
 
 void printCost(double price, String itemName) {//arguments are not optional
   print("The cost of $itemName is: $price"); 
+}
+void printCost1(String name, double? price, [String? itemName]) {//arguments are  optional
+  print("Hey $name the cost of $itemName is: $price"); 
 }
