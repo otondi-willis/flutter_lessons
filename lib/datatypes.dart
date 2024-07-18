bb99d8c288618ffc6aa90c407535b20ae1856b4b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 21:48:39 +0300

    Maps

--- lib/datatypes.dart
+++ lib/datatypes.dart
@@ -1,5 +1,16 @@
 void main() {
-  List<String> bookTitles = ["Book 1", "Book 2", "Book 3"];
+  //Lists
+  List<String>? bookTitles = ["Book 1", "Book 2", "Book 3"];
   //bookTitles.add(2);
-  print(bookTitles); // list of dynamic(can hold a value of any datatype)
+  print(bookTitles.length); // list of dynamic(can hold a value of any datatype)
+
+
+  //map
+  Map<String, String>? bookDetails = {
+    "Book 1": "Author 1",
+    "Book 2": "Author 2",
+    "Book 3": "Author 3"
+  };
+  print(bookDetails); // map of fixed datatype(both key and value)
+
 }
