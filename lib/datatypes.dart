429fb75ef66a55ab003a90c2727b4b459c123a26
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 21:41:49 +0300

    Lists without restricting datatype

--- lib/datatypes.dart
+++ lib/datatypes.dart
@@ -1,4 +1,5 @@
 void main() {
   List bookTitles = ["Book 1", "Book 2", "Book 3"];
-  print(bookTitles.runtimeType);// list of dynamic
+  print(bookTitles.runtimeType);// list of dynamic(can hold a value of any datatype)
+  
 }
