5b101d7cbd1c347bde65803b57d8e517d8c4e029
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-18 22:20:17 +0300

    use of functions with optional non positional arguments

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,12 +1,28 @@
 void main(List<String> args) {
   print("Hello world");
   printCost(10.5, "Chocolate Bar");
-  printCost1("Willis",10.5);
+  printCost1("Willis", 10.5);
+  printCost3(price: 10.5,itemName:  "Chocolate Bar");
 }
 
-void printCost(double price, String itemName) {//arguments are not optional
-  print("The cost of $itemName is: $price"); 
+//Positional parameters
+void printCost(double price, String itemName) {
+  //arguments are not optional
+  print("The cost of $itemName is: $price");
 }
-void printCost1(String name, double? price, [String? itemName]) {//arguments are  optional
-  print("Hey $name the cost of $itemName is: $price"); 
+
+void printCost1(String name, double? price, [String? itemName]) {
+  //arguments are  optional
+  print("Hey $name the cost of $itemName is: $price");
+}
+
+//Non Positional parameters
+void printCost2(double price, String itemName) {
+  //arguments are not optional
+  print("The cost of $itemName is: $price");
+}
+
+void printCost3({String? name, double? price, String? itemName}) {
+  //arguments are  optional
+  print("Hey $name the cost of $itemName is: $price");
 }
