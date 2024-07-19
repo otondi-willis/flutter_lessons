bf4f5a98890f5940f9a06a8901d441c913c25050
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-19 22:27:53 +0300

    use of material app, scaffold properties

---
+++ lib/flutter_drills/building_blocks.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Chat App'),
+        ),
+        drawer: Drawer(),
+        
+       
+      ),
+    );
+  }
+}
