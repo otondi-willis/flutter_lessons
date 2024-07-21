52a9e5ce3898b3e23438b824602458a5ec69eaf5
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-21 14:06:28 +0300

    wrapped the column with a center widget

--- lib/flutter_drills/login_page.dart
+++ lib/flutter_drills/login_page.dart
@@ -6,50 +6,28 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Login Page'),
-          ),
-        drawer: Drawer(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("Button clicked");
-          },
-        ),
-      
-      body: Column(
-        children: [
-          Text('Lets sign you in', 
-      style: TextStyle(
-        fontSize: 30, 
-        color: Colors.brown,
-        fontWeight: FontWeight.bold, 
-        letterSpacing: 0.5),),
-          Text('Welcome back! \n You\'ve been missed!',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            color: Colors.blueGrey),
-            ),
-            Image.network('https://via.placeholder.com/200.png', 
-              height: 200,),
-              Container(
-                height: 200,
-                width: 200,
+        
+      body: Center(
+        child: Column(
+          children: [
+            Text('Lets sign you in', 
+        style: TextStyle(
+          fontSize: 30, 
+          color: Colors.brown,
+          fontWeight: FontWeight.bold, 
+          letterSpacing: 0.5),),
+            Text('Welcome back! \n You\'ve been missed!',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.blueGrey),
+              ),
+              Image.network('https://via.placeholder.com/200.png', 
+                height: 200,),
                 
-                //child: FlutterLogo(),
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fitHeight,
-                    image: NetworkImage('https://via.placeholder.com/150.png'),
-                  ),
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              )
-
-        ],
+        
+          ],
+        ),
       ),
     );
   }
