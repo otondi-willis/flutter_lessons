867495a35961bb7eb4e2878357af75a8e4c3ec9e
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-21 13:27:17 +0300

    additional appbar properties used

--- lib/flutter_drills/chatpage.dart
+++ lib/flutter_drills/chatpage.dart
@@ -7,8 +7,15 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text('Hi Willis', style: TextStyle(color: Colors.blueGrey),),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.logout))
+          IconButton(
+              onPressed: () {
+                print('icon pressed');
+              },
+              icon: Icon(Icons.logout))
         ],
       ),
       body: Container(),
