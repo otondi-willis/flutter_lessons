ca2f2c5c1834343d6616610ddd56eab4edbc6896
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-21 15:09:49 +0300

    creation of chat_input class

---
+++ lib/flutter_drills/widgets/chat_input.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ChatInput extends StatelessWidget {
+  const ChatInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add, color: Colors.white),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.send, color: Colors.white),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+    );
+  }
+}
