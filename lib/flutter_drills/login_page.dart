9871b4f691f19b9841724083a3b3ac9ed4428a38
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-23 22:06:13 +0300

    use of elevated buttons properties

--- lib/flutter_drills/login_page.dart
+++ lib/flutter_drills/login_page.dart
@@ -6,32 +6,44 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Lets sign you in', 
-            
-        style: TextStyle(
-          fontSize: 30, 
-          color: Colors.brown,
-          fontWeight: FontWeight.bold, 
-          letterSpacing: 0.5),),
-            Text('Welcome back! \n You\'ve been missed!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.blueGrey),
-              ),
-              Image.network('https://via.placeholder.com/200.png', 
-                height: 200,),
-                
-               ElevatedButton(onPressed: (){}, child: Text('')),
-                OutlinedButton(onPressed: (){}, child: FlutterLogo()),
-                TextButton(onPressed: (){},child: Text('Textbutton'),),
+            Text(
+              'Lets sign you in',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.brown,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5),
+            ),
+            Text(
+              'Welcome back! \n You\'ve been missed!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.blueGrey),
+            ),
+            Image.network(
+              'https://via.placeholder.com/200.png',
+              height: 200,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  print('Clicked');
+                },
+                child: Text(
+                  'Click me!',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                )),
+            OutlinedButton(onPressed: () {}, child: FlutterLogo()),
+            TextButton(
+              onPressed: () {},
+              child: Text('Textbutton'),
+            ),
           ],
         ),
       ),
