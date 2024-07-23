bc50e044911e36e7a1dc1a13ac745b4ecfc82f31
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-23 22:20:38 +0300

    additional button properties

--- lib/flutter_drills/login_page.dart
+++ lib/flutter_drills/login_page.dart
@@ -11,42 +11,48 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Lets sign you in',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5),
-            ),
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueGrey),
-            ),
-            Image.network(
-              'https://via.placeholder.com/200.png',
-              height: 200,
-            ),
-            ElevatedButton(
-                onPressed: loginUser,
-                child: Text(
-                  'Click me!',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
-                )),
-            OutlinedButton(onPressed: () {}, child: FlutterLogo()),
-            TextButton(
-              onPressed: () {},
-              child: Text('Textbutton'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Lets sign you in',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5),
+              ),
+              Text(
+                'Welcome back! \n You\'ve been missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey),
+              ),
+              Image.network(
+                'https://via.placeholder.com/200.png',
+                height: 200,
+              ),
+              ElevatedButton(
+                
+                  onPressed: loginUser,
+                  child: Text(
+                    'Click me!',
+                
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+                  )),
+              OutlinedButton(onPressed: () {}, child: FlutterLogo()),
+              TextButton(
+                onPressed: () {},
+                child: Text('Textbutton'),
+              ),
+            ],
+          ),
         ),
       ),
     );
