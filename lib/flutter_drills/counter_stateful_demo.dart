2d40e98e4886fd5267b34b5430e777e5fd745d89
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-23 06:05:27 +0300

    Counter stateful widget formation

---
+++ lib/flutter_drills/counter_stateful_demo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CounterStateful extends StatefulWidget {
+  const CounterStateful({super.key});
+
+  @override
+  State<CounterStateful> createState() => _CounterStatefulState();
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
