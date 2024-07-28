96b248e3578160e7aae47ab38caa3ad392baa648
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-28 12:11:53 +0300

    use of factory class

--- lib/models/chat_message_entity.dart
+++ lib/models/chat_message_entity.dart
@@ -10,9 +10,21 @@ class ChatMessageEntity {
       required this.createdAt,
       this.imageUrl,
       required this.author});
+
+  factory ChatMessageEntity.fromJson(Map<String, dynamic> json) {
+    return ChatMessageEntity(
+        text: json["text"],
+        id: json["id"],
+        createdAt: json["createdAt"],
+        author: json["author"]);
+  }
 }
 
 class Author {
   String username;
   Author({required this.username});
+
+  factory Author.fromJson(Map<String, dynamic> json) {
+    return Author(username: json['username']);
+  }
 }
