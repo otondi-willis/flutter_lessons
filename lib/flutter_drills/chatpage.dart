60c36bbda8eb15c28c572c2e1d4d41766121e426
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-28 11:20:56 +0300

    created a mock messages list

--- lib/flutter_drills/chatpage.dart
+++ lib/flutter_drills/chatpage.dart
@@ -7,6 +7,32 @@ import 'package:flutter_lessons/models/chat_message_entity.dart';
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+      id: '1234',
+      text: 'Hello this is Willis',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(username: 'Willinski'),
+    ),
+    ChatMessageEntity(
+      id: '5678',
+      text: 'Hi Willis, how are you?',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(username: 'Willinski'),
+    ),
+    ChatMessageEntity(
+      id: '9101',
+      text: 'I am fine, thank you! How about you?',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(username: 'Willinski'),
+    ),
+    ChatMessageEntity(
+      id: '1314',
+      text: 'I am also good, and you?',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(username: 'Willinski'),),
+  ];
+
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
@@ -43,8 +69,6 @@ class ChatPage extends StatelessWidget {
                           text: 'Hello this is willis',
                           createdAt: DateTime.now().millisecondsSinceEpoch,
                           author: Author(username: 'Willinski'),
-
-
                         ));
                   })),
           ChatInput(),
