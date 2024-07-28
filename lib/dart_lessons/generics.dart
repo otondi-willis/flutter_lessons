99ec36405147d6b60164097bb98bf17b79057c36
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-28 10:54:15 +0300

    error prompts due to use of generics

--- lib/dart_lessons/generics.dart
+++ lib/dart_lessons/generics.dart
@@ -1,4 +1,8 @@
-void main(){
-  List fruits = ["apple","orange"];
+void main() {
+  List<String> fruits = ["apple", "orange"];
   print(fruits.runtimeType);
+
+  //calls for the essence of generics List<>
+  fruits.add(true);
+  fruits.add(2);
 }
