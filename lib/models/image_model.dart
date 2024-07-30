1a8566d2e2a8907ea4b9717afbad080d4b854b15
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-30 21:19:17 +0300

    pixelformimage constructor created

--- lib/models/image_model.dart
+++ lib/models/image_model.dart
@@ -1,3 +1,14 @@
 class PixelformImage {
-  
+  String id;
+  String filename;
+  String? title;
+  String urlFullSize;
+  String urlSmallSize;
+
+  PixelformImage(
+      {required this.id,
+      required this.filename,
+      this.title,
+      required this.urlFullSize,
+      required this.urlSmallSize});
 }
