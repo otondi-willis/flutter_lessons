f38af75b01db224288f95ea643fa1fe37525236b
otondi-willis <willis.otondi.ke@gmail.com>
2024-07-31 05:18:16 +0300

    api connection logic transferred to repository pattern

--- lib/flutter_drills/chatpage.dart
+++ lib/flutter_drills/chatpage.dart
@@ -7,7 +7,7 @@ import 'package:flutter_lessons/flutter_drills/widgets/chat_bubble.dart/chat_bub
 import 'package:flutter_lessons/flutter_drills/widgets/chat_input.dart';
 import 'package:flutter_lessons/models/chat_message_entity.dart';
 import 'package:flutter_lessons/models/image_model.dart';
-import 'package:http/http.dart' as http;
+
 
 class ChatPage extends StatefulWidget {
   ChatPage({super.key});
@@ -46,25 +46,7 @@ class _ChatPageState extends State<ChatPage> {
     setState(() {});
   }
 
-  //connection to an api
-  //move this to a repository pattern
-  //repositories are classes that encapsulate or contain the logic required to access data sources
-  Future<List<PixelformImage>> _getNetworkImages() async {
-    var endpointUrl = Uri.parse('https://pixelford.com/api/img/small');
-    final response = await http.get(endpointUrl);
-
-    if (response.statusCode == 200) {
-      final decodedList = jsonDecode(response.body) as List;
-      final List<PixelformImage> _imageList = decodedList.map((ListItem) {
-        return PixelformImage.fromJson(ListItem);
-      }).toList();
-
-      print(_imageList[0].urlFullSize);
-      return _imageList;
-    } else {
-      throw Exception('API not successful');
-    }
-  }
+  
 
   @override
   void initState() {
