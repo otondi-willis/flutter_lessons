580efb49660a2b2576793803fba551b842bfd795
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-01 05:55:39 +0300

    imported image model

--- lib/flutter_drills/widgets/picker_body.dart
+++ lib/flutter_drills/widgets/picker_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lessons/models/image_model.dart';
 
 class NetworkImagePickerBody extends StatelessWidget {
   const NetworkImagePickerBody({super.key});
