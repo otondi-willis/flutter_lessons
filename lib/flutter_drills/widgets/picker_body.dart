a208bc4196c5d43aa164fe3f3dc132fe9d8890c8
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-01 06:08:58 +0300

    adding list view builder

--- lib/flutter_drills/widgets/picker_body.dart
+++ lib/flutter_drills/widgets/picker_body.dart
@@ -3,19 +3,20 @@ import 'package:flutter_lessons/models/image_model.dart';
 import 'package:flutter_lessons/repo/image_repository.dart';
 
 class NetworkImagePickerBody extends StatelessWidget {
- 
-   NetworkImagePickerBody({super.key});
+  NetworkImagePickerBody({super.key});
 
- final ImageRepository _imageRepo;
+  final ImageRepository _imageRepo;
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<PixelformImage>>(
-              future: _imageRepo.getNetworkImages(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<PixelformImage>> snapshot) {
-                if (snapshot.hasData)
-                  return Image.network(snapshot.data![0].urlSmallSize);
-                return CircularProgressIndicator();
-              });
+        future: _imageRepo.getNetworkImages(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<PixelformImage>> snapshot) {
+          if (snapshot.hasData) {
+            ListView.builder(itemBuilder: itemBuilder)
+            return Image.network(snapshot.data![0].urlSmallSize);
+          }
+          return CircularProgressIndicator();
+        });
   }
 }
