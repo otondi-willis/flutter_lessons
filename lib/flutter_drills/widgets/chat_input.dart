5e47d17df9b62416fb7aaff36da921c3d231334e
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-01 13:00:12 +0300

    textfield wrapped with a column

--- lib/flutter_drills/widgets/chat_input.dart
+++ lib/flutter_drills/widgets/chat_input.dart
@@ -39,18 +39,22 @@ class ChatInput extends StatelessWidget {
             icon: Icon(Icons.add, color: Colors.white),
           ),
           Expanded(
-              child: TextField(
-            keyboardType: TextInputType.multiline,
-            maxLines: 5,
-            minLines: 1,
-            controller: chatMessageController,
-            textCapitalization: TextCapitalization.sentences,
-            style: TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-                hintText: "Type your message",
-                hintStyle: TextStyle(color: Colors.blueGrey),
-                border: InputBorder.none),
-          )),
+              child: Column(
+                children: [
+                  TextField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 5,
+                              minLines: 1,
+                              controller: chatMessageController,
+                              textCapitalization: TextCapitalization.sentences,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                    hintText: "Type your message",
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: InputBorder.none),
+                            ),
+                ],
+              )),
           IconButton(
             onPressed: onSendButtonPressed,
             icon: Icon(Icons.send, color: Colors.white),
