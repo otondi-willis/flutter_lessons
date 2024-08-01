6661f97c30ee98a1a790cb89499068d2e80d42c1
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-01 05:51:58 +0300

    created stateless NetworkImagePickerBody

---
+++ lib/flutter_drills/widgets/picker_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NetworkImagePickerBody extends StatelessWidget {
+  const NetworkImagePickerBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
