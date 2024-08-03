23d5c7e791f3acfbf3e165a346a6f95dff6fc670
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-03 10:50:12 +0300

    added container decoration

--- lib/flutter_drills/widgets/picker_body.dart
+++ lib/flutter_drills/widgets/picker_body.dart
@@ -9,36 +9,43 @@ class NetworkImagePickerBody extends StatelessWidget {
       : _imageRepo = ImageRepository();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<PixelformImage>>(
-        future: _imageRepo.getNetworkImages(),
-        builder: (BuildContext context,
-            AsyncSnapshot<List<PixelformImage>> snapshot) {
-          if (snapshot.hasData) {
-            return GridView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                    onTap: () {
-                      onImageSelected(snapshot.data![index].urlSmallSize);
-                    },
-                    child: Image.network(snapshot.data![index].urlSmallSize));
-              },
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  crossAxisSpacing: 2,
-                  mainAxisExtent: 2,
-                  maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5),
-            );
-            //
-          } else if (snapshot.hasError) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24))
+      ),
+      child: FutureBuilder<List<PixelformImage>>(
+          future: _imageRepo.getNetworkImages(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<PixelformImage>> snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                      onTap: () {
+                        onImageSelected(snapshot.data![index].urlSmallSize);
+                      },
+                      child: Image.network(snapshot.data![index].urlSmallSize));
+                },
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    crossAxisSpacing: 2,
+                    mainAxisExtent: 2,
+                    maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5),
+              );
+              //
+            } else if (snapshot.hasError) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('This is the error: ${snapshot.error}'),
+              );
+            }
             return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('This is the error: ${snapshot.error}'),
+              padding: const EdgeInsets.all(24.0),
+              child: Center(child: CircularProgressIndicator()),
             );
-          }
-          return Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Center(child: CircularProgressIndicator()),
-          );
-        });
+          }),
+    );
   }
 }
