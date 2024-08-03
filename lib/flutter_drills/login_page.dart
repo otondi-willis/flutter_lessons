ec6c4b6fd6a9cb8a32e931797e1a82ab1cee6236
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-03 10:54:18 +0300

    login page background color of white

--- lib/flutter_drills/login_page.dart
+++ lib/flutter_drills/login_page.dart
@@ -37,6 +37,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
