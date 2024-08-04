941524297c8017a10449954d70396f526293eb85
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-04 11:39:29 +0300

    update color depending on author

--- lib/flutter_drills/widgets/chat_bubble.dart/chat_bubble.dart
+++ lib/flutter_drills/widgets/chat_bubble.dart/chat_bubble.dart
@@ -3,31 +3,30 @@ import 'package:flutter_lessons/models/chat_message_entity.dart';
 
 class ChatBubble extends StatelessWidget {
   final ChatMessageEntity entity;
- final Alignment alignment;
+  final Alignment alignment;
   const ChatBubble({super.key, required this.alignment, required this.entity});
 
+  
   @override
   Widget build(BuildContext context) {
+    bool isAuthor = entity.author.username == "willis";
     return Align(
       alignment: alignment,
       child: Container(
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.5),
-        
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.5),
         padding: EdgeInsets.all(12),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text('${entity.text}', style: TextStyle(fontSize: 20, color: Colors.white)),
-            if(entity.text!= null)
-              Image.network('${entity.imageUrl}'),
-            
-            
+            Text('${entity.text}',
+                style: TextStyle(fontSize: 20, color: Colors.white)),
+            if (entity.text != null) Image.network('${entity.imageUrl}'),
           ],
         ),
         margin: EdgeInsets.all(50),
         decoration: BoxDecoration(
-          color: Colors.grey,
+          color: isAuthor ? Theme.of(context).primaryColor : Colors.red,
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(12),
               topRight: Radius.circular(12),
