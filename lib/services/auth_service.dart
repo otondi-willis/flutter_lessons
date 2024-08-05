e2b7be42ab7c41de65dfc27a6bee12b8c0bd745c
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-05 21:25:44 +0300

    login functionality

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,16 +2,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-  Future <void> loginUser(String userName) async {
-    try{
-    SharedPreferences sharedPrefs = await _prefs;
-    sharedPrefs.setString('userName', userName);
+  Future<void> loginUser(String userName) async {
+    try {
+      SharedPreferences sharedPrefs = await _prefs;
+      sharedPrefs.setString('userName', userName);
     } catch (e) {
       print('Error logging in: $e');
-   }
+    }
   }
 
-  void logoutUser() {}
+  void logoutUser() async {
+    SharedPreferences sharedPrefs = await _prefs;
+    sharedPrefs.clear();
+  }
 
   String getUserName() {
     return "Willis";
