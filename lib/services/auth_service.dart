b56590aa7c8326e3c26401c388a8d4171cb82248
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-05 21:16:17 +0300

    shared preferences set to string

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,7 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthService {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   Future <void> loginUser(String userName) async {
-    SharedPreferences prefs = await _prefs;
+    SharedPreferences sharedPrefs = await _prefs;
+    sharedPrefs.setString('userName', userName)
   }
 
   void logoutUser() {}
