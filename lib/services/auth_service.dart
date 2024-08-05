34c7c4df1cdc54249fcd2fb3766edc78455fc5d8
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-05 21:24:14 +0300

    use of try catch block

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,8 +3,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthService {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   Future <void> loginUser(String userName) async {
+    try{
     SharedPreferences sharedPrefs = await _prefs;
-    sharedPrefs.setString('userName', userName)
+    sharedPrefs.setString('userName', userName);
+    } catch (e) {
+      print('Error logging in: $e');
+   }
   }
 
   void logoutUser() {}
