8f99aa553b13be1b2b2839e3aa34f9a633d1fed5
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-05 21:11:48 +0300

    creation of future shared preferences

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthService {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   void loginUser() {}
   void logoutUser() {}
 
