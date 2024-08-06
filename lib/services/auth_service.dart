947f3487d2a99c87b5b343483b6ce2ede3b40bd5
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-06 22:16:21 +0300

    part 4

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,25 +1,29 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  static init() async {
+  static late SharedPreferences _prefs;
+
+  static Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
-    //await SharedPreferences.getInstance();
   }
 
-  static late final Future<SharedPreferences> _prefs;
   Future<void> loginUser(String userName) async {
     try {
-      _prefs.setString('userName', userName);
+      await _prefs.setString('userName', userName);
     } catch (e) {
       print('Error logging in: $e');
     }
   }
 
-  void logoutUser() {
-    _prefs.clear();
+  Future<void> logoutUser() async {
+    try {
+      await _prefs.clear();
+    } catch (e) {
+      print('Error logging out: $e');
+    }
   }
 
-  String? getUserName() {
+  Future<String?> getUserName() async {
     return _prefs.getString('userName') ?? "Default Value";
   }
 }
